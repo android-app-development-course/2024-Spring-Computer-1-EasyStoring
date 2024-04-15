52963c11e44ab20e8d714862c7a9d0e2e0129f78
Coconut <1750505884@qq.com>
2024-04-15 11:32:22 +0800

    CL

--- EasyStoring/app/src/main/java/com/example/easystoring/MainActivity.kt
+++ EasyStoring/app/src/main/java/com/example/easystoring/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.easystoring
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.text.set
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val b2: Button = findViewById(R.id.button2)
+        val i1: EditText = findViewById(R.id.editText)
+        b2.setOnClickListener {
+            var t = i1.text.toString()
+            t = t + t
+            i1.text.clear()
+            i1.text.append(t)
+        }
     }
 }
