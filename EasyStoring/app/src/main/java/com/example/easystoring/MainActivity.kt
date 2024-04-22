b8fed64ebf47dd993cfe934ff6f0faa399109946
Coconut <1750505884@qq.com>
2024-04-22 11:41:13 +0800

    Update MainActivity.kt

--- EasyStoring/app/src/main/java/com/example/easystoring/MainActivity.kt
+++ EasyStoring/app/src/main/java/com/example/easystoring/MainActivity.kt
@@ -10,11 +10,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val binding=ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+
         val b2: Button = findViewById(R.id.button2)
         val i1: EditText = findViewById(R.id.editText)
         b2.setOnClickListener {
             var t = i1.text.toString()
-            t = t + t
+            t += t
             i1.text.clear()
             i1.text.append(t)
         }
