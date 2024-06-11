5c430603a80ffa25763f295f44ce4337ba941e24
Coconut <1750505884@qq.com>
2024-06-11 22:24:01 +0800

    CL

--- EasyStoring/app/src/main/java/com/example/easystoring/ui/TestFragment/TestFragment.kt
+++ EasyStoring/app/src/main/java/com/example/easystoring/ui/TestFragment/TestFragment.kt
@@ -8,8 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.easystoring.databinding.FragmentTestBinding
-import com.example.easystoring.logic.model.NetworkService
+import com.example.easystoring.logic.network.NetworkService
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
@@ -39,20 +40,50 @@ class TestFragment : Fragment() {
 
         binding.button.setOnClickListener {
             val map = mutableMapOf<String, String>()
-            map["username"]="Cecilia"
-            map["password"]="12412344"
-            val jsonString=Gson().toJson(map)
+            map["username"] = "Cecilia"
+            map["password"] = "12412344"
+            val jsonString = Gson().toJson(map)
 //            val request = Request.Builder().url(url).header("UserInformation",map.toString()).get().build()
-            val jsonBody=
+            val jsonBody =
                 jsonString.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
-            val jsonRequest=Request.Builder().url(url).post(jsonBody).build()
+            val jsonRequest = Request.Builder().url(url).post(jsonBody).build()
             val call = NetworkService.httpClient.newCall(jsonRequest)
-            Log.d("2333","jsonString${jsonString}")
-            Log.d("2333","jsonBody$jsonBody")
             thread {
                 try {
                     val response = call.execute()
                     response.body?.string()?.let { it1 -> Log.d("2333", it1) }
+                } catch (e: IOException) {
+                    e.message?.let { Log.d("2333", it) }
+                }
+            }
+        }
+
+        binding.button3.setOnClickListener {
+//            val map = mutableMapOf<String, String>()
+//            map["username"]="Cecilia"
+//            map["password"]="12412344"
+//            val jsonString=Gson().toJson(map)
+////            val request = Request.Builder().url(url).header("UserInformation",map.toString()).get().build()
+//            val jsonBody=
+//                jsonString.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+//            val jsonRequest=Request.Builder().url(url).post(jsonBody).build()
+//            val call = NetworkService.httpClient.newCall(jsonRequest)
+            val getRequest =
+                Request.Builder().url(NetworkService.baseURL + "/getlist").get().build()
+            val getCall = NetworkService.httpClient.newCall(getRequest)
+
+            thread {
+                try {
+                    val response = getCall.execute()
+                    response.body?.string()?.let {
+                        Log.d("2333", it)
+                        val map: MutableMap<*, *> = Gson().fromJson(
+                            it,
+                            MutableMap::class.java
+                        )
+                        for ((key,value ) in map)
+                            Log.d("2333","${key!!::class.java} ${value!!::class.java}")
+                    }
                 } catch (e: IOException) {
                     e.message?.let { Log.d("2333", it) }
                 }
